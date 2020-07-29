2cdcc1b5a6de225b501c0fcce4dd31d97fe457e9
anisurRahman1 <fourangle1@gmail.com>
2020-07-29 23:34:52 +0600

    first project

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default   {
+    products:[
+        {
+            _id:'1',
+            name:'Tangail sari',
+            category:'sari',
+            image:'/images/p1.jpg',
+            price:6000,
+            brand:'tangail',
+            rating:4.5,
+            numReviews:10
+
+        },
+        {
+            _id:'2',
+            name:'Legal sari',
+            category:'sari',
+            image:'/images/p1.jpg',
+            price:5550,
+            brand:'tangail',
+            rating:4.5,
+            numReviews:10
+
+        },
+        {
+            _id:'3',
+            name:'unknown sari',
+            category:'sari',
+            image:'/images/p1.jpg',
+            price:6670,
+            brand:'tangail',
+            rating:4.5,
+            numReviews:10
+
+        },
+        {
+            _id:'4',
+            name:'unknown sari',
+            category:'sari',
+            image:'/images/p1.jpg',
+            price:6390,
+            brand:'tangail',
+            rating:4.5,
+            numReviews:10
+
+        },
+    ]
+}
